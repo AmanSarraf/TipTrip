27f28aff3a1bd549c968097f2bc5bfe4ee17ee8e
AmanSarraf <amansarrafnit@gmail.com>
2023-07-30 13:36:13 +0530

    Fixed Models

--- backend/controllers/users.js
+++ backend/controllers/users.js
@@ -1,4 +1,4 @@
-const User = require("../model/user");
+const User = require("../models/user");
 const userRouter = require("express").Router();
 
 userRouter.post("/", async (request, response) => {
