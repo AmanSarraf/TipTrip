6bde28f0013553c8b2845e5f56a91bb4d37c1ebe
Chaman Rawat <chamanrawat.dev@gmail.com>
2023-07-30 20:26:07 +0530

    add itineraries post route

--- backend/app.js
+++ backend/app.js
@@ -32,12 +32,11 @@ app.use(express.static("build"));
 app.use(express.json());
 app.use(middleware.requestLogger);
 app.use(middleware.tokenExtractor);
-app.use(middleware.userExtractor);
 
+app.use("/api/itinerary", middleware.userExtractor, itineraryRouter);
 app.use("/api/places", placesRouter);
 app.use("/api/login", loginRouter);
 app.use("/api/users", usersRouter);
-app.use("/api/itinerary", itineraryRouter);
 
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
