16d67fdb45d351441adce9471168e4adbc045005
Chaman Rawat <chamanrawat.dev@gmail.com>
2023-07-30 16:54:33 +0530

    remove routes/

--- backend/routes/home.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { home } = require("../controller/homeController");
-
-//what happens when someone visit get route : this is where we bring controller
-router.route("/").get(home);
-
-module.exports = router;
